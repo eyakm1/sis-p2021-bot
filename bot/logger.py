90e28032bf11e9d7328c37b635657fb41d91c6b1
Anton Shumilin <shumilinantony@gmail.com>
2021-07-22 08:31:17 +0000

    bot: fix not existing log directory in logger.py
    - Try creating directory for logs
    - Add typehints
    - Use American-style personalize_logs instead of personalise_logs

--- bot/logger.py
+++ bot/logger.py
@@ -6,15 +6,16 @@ from config import LOG_FILE_UPDATE_INTERVAL_HOURS, LOGS_BACKUP_FILE_COUNT
 os.makedirs("logs", exist_ok=True)
 
 
-def get_logger(name):
+def get_logger(name: str) -> logging.Logger:
     logger = logging.getLogger(name)
-    return personalise_logger(logger, name)
+    return personalize_logger(logger, name)
 
 
-def personalise_logger(logger, name):
+def personalize_logger(logger: logging.Logger, name: str) -> logging.Logger:
+    os.makedirs(f"logs/{name}", exist_ok=True)
     logger.setLevel(logging.DEBUG)
     logger_file_handler = logging.handlers.TimedRotatingFileHandler(
-        f"bot/logs/{name}/{name}.log",
+        f"logs/{name}/{name}.log",
         encoding="utf-8", when="h", interval=LOG_FILE_UPDATE_INTERVAL_HOURS,
         backupCount=LOGS_BACKUP_FILE_COUNT)
     logging_formatter = logging.Formatter("%(levelname)s %(asctime)s - %(message)s")
@@ -24,4 +25,4 @@ def personalise_logger(logger, name):
 
 
 telebot_logger = telebot.logger
-telebot_logger = personalise_logger(telebot_logger, "telebot")
+telebot_logger = personalize_logger(telebot_logger, "telebot")

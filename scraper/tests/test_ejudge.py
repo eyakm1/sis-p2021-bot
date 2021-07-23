903d16d67170bb05b9cf681ab1d1307f2cd0a3fe
Pavel Kalugin <pavel@pavelthebest.me>
2021-07-23 15:38:00 +0300

    scraper: fix ejudge last run id filter
    Ejudge may send incorrect page on filter, so we need to filter
    submissions for good last run id manually
    
    - Add test for that case
    - Fix default last run id to be -1, not NULL
    
    Signed-off-by: Pavel Kalugin <pavel@pavelthebest.me>

--- scraper/tests/test_ejudge.py
+++ scraper/tests/test_ejudge.py
@@ -17,10 +17,20 @@ def test_many_users_with_symbols_after_run_id():
                                                           status_code=200)):
         parser = ej_parser.ContestParser(contest_id=1)
         pr_submissions = parser.parse_all_new_pr()
-        print(pr_submissions)
         assert pr_submissions == [{'login': 'abishev', 'problem': 'B', 'rid': 5, 'cid': 1},
                                   {'login': 'abishev', 'problem': 'A', 'rid': 4, 'cid': 1},
                                   {'login': 'ejudge', 'problem': 'B', 'rid': 3, 'cid': 1},
                                   {'login': 'ejudge', 'problem': 'A', 'rid': 1, 'cid': 1},
                                   {'login': 'ejudge', 'problem': 'A', 'rid': 0, 'cid': 1}]
+        assert parser.last_rid == 5
+
+
+def test_no_submissions():
+    with open('scraper/tests/ej_table_many_pr_with_symbols_after_run_id.html', 'rb') \
+            as table_html, patch.object(requests.Session, 'get',
+                                        return_value=Mock(content=table_html.read(),
+                                                          status_code=200)):
+        parser = ej_parser.ContestParser(contest_id=1, last_rid=5)
+        pr_submissions = parser.parse_all_new_pr()
+        assert pr_submissions == []
         assert parser.last_rid == 5

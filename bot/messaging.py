a8879cf8417317c2046d67ad8d75dfa77e233805
Anton Shumilin <shumilinantony@gmail.com>
2021-07-24 13:55:38 +0300

    fixed hashtag

--- bot/messaging.py
+++ bot/messaging.py
@@ -1,3 +1,4 @@
+import re
 from typing import Optional
 
 from telebot import apihelper
@@ -7,6 +8,11 @@ from bot.logger import get_logger
 from bot.submission import Submission
 
 notification_logger = get_logger("notifications")
+
+
+def prepare_for_hashtag(s: str):
+    replaced = re.sub(r'\W', '_', s)  # replace non-alphanumeric symbols with _
+    return re.sub(r'_+', '_', replaced)  # merge consequent underscores
 
 
 def generate_notify_markup(callback_data: int) -> InlineKeyboardMarkup:
@@ -25,8 +31,8 @@ def generate_individual_markup(callback_data: int) -> InlineKeyboardMarkup:
 
 
 def generate_message(submission: Submission) -> str:
-    return f"#contest_{submission.cid}, #problem_{submission.problem}, " \
-           f"#run_id_{submission.rid}, #user_{submission.login}"
+    return f"#contest_{submission.cid}, #problem_{prepare_for_hashtag(submission.problem)}, " \
+           f"#run_id_{submission.rid}, #user_{prepare_for_hashtag(submission.login)}"
 
 
 def send(bot, chat_id: int, message: str, markup: InlineKeyboardMarkup = None) -> Optional[int]:

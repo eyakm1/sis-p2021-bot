a137014c33fd05b86c27f17cbbf39d40b95df28b
Anton Shumilin <shumilinantony@gmail.com>
2021-07-25 07:19:00 +0000

    Fixed login hashtag

--- bot/messaging.py
+++ bot/messaging.py
@@ -1,19 +1,23 @@
 import re
 from typing import Optional
+
 from telebot import apihelper
 from telebot.types import InlineKeyboardMarkup, InlineKeyboardButton
+
 from bot.logger import get_logger
 from bot.submission import Submission
 
 notification_logger = get_logger("notifications")
 
 
-def prepare_for_hashtag(s: str):
+def prepare_for_hashtag(s: str, prefix: str = '') -> str:
+    if s.isdigit() or s == '':
+        return prefix + s
     replaced = re.sub(r'\W', '_', s)  # replace non-alphanumeric symbols with _
     return re.sub(r'_+', '_', replaced)  # merge consequent underscores
 
 
-def generate_notify_markup(callback_data: int) -> InlineKeyboardMarkup:
+def generate_group_markup(callback_data: int) -> InlineKeyboardMarkup:
     markup = InlineKeyboardMarkup()
     markup.add(InlineKeyboardButton("\U00002705 Взять", callback_data=f"assigned {callback_data}"))
     return markup
@@ -29,8 +33,9 @@ def generate_individual_markup(callback_data: int) -> InlineKeyboardMarkup:
 
 
 def generate_message(submission: Submission) -> str:
-    return f"#c{submission.cid}, #{submission.problem}, " \
-           f"run id: {submission.rid}, #{submission.login}"
+    return f"#{prepare_for_hashtag(str(submission.cid), 'c')}, " \
+           f"#{prepare_for_hashtag(submission.problem, 'p')}, " \
+           f"#{prepare_for_hashtag(submission.login, 'u')}"
 
 
 def send(bot, chat_id: int, message: str, markup: InlineKeyboardMarkup = None) -> Optional[int]:
@@ -44,7 +49,7 @@ def send(bot, chat_id: int, message: str, markup: InlineKeyboardMarkup = None) -
 
 
 def send_to_group(bot, submission: Submission, chat_id: int) -> Optional[int]:
-    markup = generate_notify_markup(submission.id)
+    markup = generate_group_markup(submission.id)
     message = generate_message(submission)
     return send(bot, chat_id, message, markup)
 
